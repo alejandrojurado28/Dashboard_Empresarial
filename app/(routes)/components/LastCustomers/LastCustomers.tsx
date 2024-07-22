a3789d79f435ab08e898b295bf51ce7fd4860802
alejandrojurado28 <147718462+alejandrojurado28@users.noreply.github.com>
2024-07-22 17:00:58 +0200

    Inicio del proyecto, sidebar, navbar y maquetando la página home

---
+++ app/(routes)/components/LastCustomers/LastCustomers.tsx
@@ -0,0 +1,16 @@
+import CustomIcon from "@/components/CustomIcon/CustomIcon";
+import { Building } from "lucide-react";
+
+export function LastCustomers() {
+  return (
+    <div className="shadow-sm bg-background rounded-lg p-5">
+        <div className="flex gap-x-2 items-center">
+            <CustomIcon icon={Building} />
+            <p className="text-xl">Last customers</p>
+        </div>
+        <div>
+            <p>Customer table</p>
+        </div>
+    </div>
+  )
+}
---
+++ app/(routes)/page.tsx
@@ -0,0 +1,55 @@
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import CardSummary from "./components/CardSummary/CardSummary";
+import { BookOpenCheck, UserRound, Waypoints } from "lucide-react";
+import { LastCustomers } from "./components/LastCustomers";
+
+export const dataCardsSummary = [
+  {
+    icon: UserRound,
+    total: "12.450",
+    average: 15,
+    title: "Companies created",
+    tooltipText: "See all of the companies created",
+  },
+  {
+    icon: Waypoints,
+    total: "86.5%",
+    average: 80,
+    title: "Total Revenue",
+    tooltipText: "See all of the summary",
+  },
+  {
+    icon: BookOpenCheck,
+    total: "363.95€",
+    average: 30,
+    title: "Bounce Rate",
+    tooltipText: "See all of the bounce rate",
+  },
+];
+
+export default function Home() {
+  return (
+    <div>
+      <h2 className="text-2xl mb-4">Dashboard</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 lg:gap-x-20">
+        {dataCardsSummary.map(
+          ({ icon, total, average, title, tooltipText }) => (
+            <CardSummary
+              key={title}
+              icon={icon}
+              total={total}
+              average={average}
+              title={title}
+              tooltipText={tooltipText}
+            />
+          )
+        )}
+      </div>
+      <div className="grid grid-cols-1 xl:grid-cols-2 md:gap-x-10 mt-12">
+        <LastCustomers />
+        <p>Sales Distibutors</p>
+      </div>
+    </div>
+  );
+}
